7ec898541656985e650fe0ce066d5e199dd018e6
Ryan <charles.ryan.mcdowell@gmail.com>
2023-12-21 21:08:51 -0500

    Initial commit

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideAnimations()
+]};
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TopMenuComponent } from './shared/top-menu/top-menu.component';
+
+export const routes: Routes = [
+    {path: 'header', component: TopMenuComponent}
+];
